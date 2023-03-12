1f5ff63047139ffe0c72b3a517c02a5133b3c61b
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-12 19:23:31 +0600

    Link is updated instead of CustomLink

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is about us</h2>
+            <p>wanna know about us?</p>
+            <p>
+                <small>Taile aga birany khawyio. Na khawile bolmu na,</small>
+            </p>
+        </div>
+    );
+};
+
+export default About;
